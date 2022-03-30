5bd3c5618a41fde360ee9f24aaee59b4cbbdcc57
Jon Lawitts <jlawitts@gmail.com>
2022-03-30 12:45:59 -0400

    add namespaces

--- Assets/OpenTerminal/Runtime/Scripts/TerminalGUI.cs
+++ Assets/OpenTerminal/Runtime/Scripts/TerminalGUI.cs
@@ -1,57 +1,60 @@
 using UnityEngine;
 
-public class TerminalGUI
+namespace OpenTerminal
 {
-    private Terminal terminal;
-    private TerminalConfig config;
-    private Texture2D texture2D;
-
-    public GUIStyle terminalStyle { get; private set; }
-
-    public TerminalGUI(Terminal terminal)
+    public class TerminalGUI
     {
-        this.terminal = terminal;
-        config = terminal.config;
-        terminalStyle = new GUIStyle();
-        terminalStyle.font = config.font;
-        terminalStyle.fontSize = config.fontSize;
-        terminalStyle.richText = true;
-        terminalStyle.normal.textColor = config.commandColor;
-        terminalStyle.hover.textColor = config.autoCompleteHoverColor;
-        terminalStyle.active.textColor = config.autoCompleteHoverColor;
-        terminalStyle.onHover.textColor = config.autoCompleteHoverColor;
-        terminalStyle.onActive.textColor = config.autoCompleteHoverColor;
+        private Terminal terminal;
+        private TerminalConfig config;
+        private Texture2D texture2D;
 
-        texture2D = new Texture2D(2, 2);
-        texture2D.SetPixels(0, 0, texture2D.width, texture2D.height, new Color[] { config.backgroundColor, config.backgroundColor, config.backgroundColor, config.backgroundColor });
-        texture2D.wrapMode = TextureWrapMode.Repeat;
-        texture2D.Apply();
+        public GUIStyle terminalStyle { get; private set; }
 
-        terminalStyle.normal.background = texture2D;
-    }
-
-    internal void OnGUI()
-    {
-        GUILayout.Label(terminal.History + terminal.ConsoleLine + terminal.InputText, terminalStyle);
-        if (terminal.AutoCompList.Count > 0)
+        public TerminalGUI(Terminal terminal)
         {
-            for (int i = 0; i < terminal.AutoCompList.Count; i++)
+            this.terminal = terminal;
+            config = terminal.config;
+            terminalStyle = new GUIStyle();
+            terminalStyle.font = config.font;
+            terminalStyle.fontSize = config.fontSize;
+            terminalStyle.richText = true;
+            terminalStyle.normal.textColor = config.commandColor;
+            terminalStyle.hover.textColor = config.autoCompleteHoverColor;
+            terminalStyle.active.textColor = config.autoCompleteHoverColor;
+            terminalStyle.onHover.textColor = config.autoCompleteHoverColor;
+            terminalStyle.onActive.textColor = config.autoCompleteHoverColor;
+
+            texture2D = new Texture2D(2, 2);
+            texture2D.SetPixels(0, 0, texture2D.width, texture2D.height, new Color[] { config.backgroundColor, config.backgroundColor, config.backgroundColor, config.backgroundColor });
+            texture2D.wrapMode = TextureWrapMode.Repeat;
+            texture2D.Apply();
+
+            terminalStyle.normal.background = texture2D;
+        }
+
+        internal void OnGUI()
+        {
+            GUILayout.Label(terminal.History + terminal.ConsoleLine + terminal.InputText, terminalStyle);
+            if (terminal.AutoCompList.Count > 0)
             {
-                GUI.SetNextControlName(terminal.AutoCompList[i]);
-                GUIStyle t = terminalStyle;
-                if (i == terminal.AutoCompIndex)
-                    GUI.color = config.autoCompleteHoverColor;
-                else
-                    GUI.color = config.commandColor;
-                if (GUILayout.Button(terminal.AutoCompList[i], t))
+                for (int i = 0; i < terminal.AutoCompList.Count; i++)
                 {
-                    terminal.ChangeInput(terminal.AutoCompList[i]);
-                    terminal.PreExecute();
+                    GUI.SetNextControlName(terminal.AutoCompList[i]);
+                    GUIStyle t = terminalStyle;
+                    if (i == terminal.AutoCompIndex)
+                        GUI.color = config.autoCompleteHoverColor;
+                    else
+                        GUI.color = config.commandColor;
+                    if (GUILayout.Button(terminal.AutoCompList[i], t))
+                    {
+                        terminal.ChangeInput(terminal.AutoCompList[i]);
+                        terminal.PreExecute();
+                    }
                 }
             }
+            else
+                GUI.color = config.commandColor;
         }
-        else
-            GUI.color = config.commandColor;
-    }
 
+    }
 }
--- Assets/OpenTerminal/Samples~/RotatingCube/RotateCube.cs
+++ Assets/OpenTerminal/Samples~/RotatingCube/RotateCube.cs
@@ -2,32 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateCube : MonoBehaviour
+namespace OpenTerminal.Examples
 {
-    private bool stop;
-    void Update()
+    public class RotateCube : MonoBehaviour
     {
-        if (!stop)
-            transform.Rotate(new Vector3(1, 1, 1));
-    }
+        private bool stop;
+        void Update()
+        {
+            if (!stop)
+                transform.Rotate(new Vector3(1, 1, 1));
+        }
 
 
-    [TerminalCommand("stop-cube", "stops the cube from rotating")]
-    public void StopCube()
-    {
-        stop = true;
-    }
+        [TerminalCommand("stop-cube", "stops the cube from rotating")]
+        public void StopCube()
+        {
+            stop = true;
+        }
 
 
-    [TerminalCommand("rotate-cube", "rotates the cube")]
-    public void RotateTheCube()
-    {
-        stop = false;
-    }
+        [TerminalCommand("rotate-cube", "rotates the cube")]
+        public void RotateTheCube()
+        {
+            stop = false;
+        }
 
-    [TerminalCommand("move-cube", "move-cube(x,y,z) Moves the cube")]
-    public void Move(float x, float y, float z)
-    {
-        transform.position = new Vector3(x, y, z);
+        [TerminalCommand("move-cube", "move-cube(x,y,z) Moves the cube")]
+        public void Move(float x, float y, float z)
+        {
+            transform.position = new Vector3(x, y, z);
+        }
     }
 }
